ac226e3046a2f87684aceeaf81da9b353fde6377
Yurii Mazurevich <JMazurevich@gmail.com>
2026-05-17 20:50:36 +0300

    Static typing: scenes/tiles/units/heroes

--- scenes/tiles/units/heroes/hero.gd
+++ scenes/tiles/units/heroes/hero.gd
@@ -1,9 +1,9 @@
 extends BaseUnit
 class_name HeroUnit
 
-var disable_active_abilities = false
+var disable_active_abilities: bool = false
 
-func register_ability(ability):
+func register_ability(ability: Ability) -> void:
     if ability.TYPE == "hero_passive":
         self.passive_ability = ability
     if ability.TYPE == "hero_active":
@@ -12,51 +12,51 @@ func register_ability(ability):
     super.register_ability(ability)
 
 
-func has_active_ability():
+func has_active_ability() -> bool:
     if self.disable_active_abilities:
         return false
 
     return self.active_abilities.size() > 0
 
-func has_passive_ability():
+func has_passive_ability() -> bool:
     return self.passive_ability != null
 
-func disable_abilities():
+func disable_abilities() -> void:
     self.disable_active_abilities = true
 
-func enable_abilities():
+func enable_abilities() -> void:
     self.disable_active_abilities = false
 
-func set_side_material(material):
+func set_side_material(material: Resource) -> void:
     if material == null:
         return
 
     super.set_side_material(material)
     $"mesh_anchor/standard".set_surface_override_material(0, material)
 
-func disable_shadow():
+func disable_shadow() -> void:
     super.disable_shadow()
 
     $"mesh_anchor/standard".cast_shadow = 0
 
-func reset_position_for_tile_view():
+func reset_position_for_tile_view() -> void:
     super.reset_position_for_tile_view()
     $"mesh_anchor/standard".hide()
 
-func get_dict():
-    var new_dict = super.get_dict()
+func get_dict() -> Dictionary[String, Variant]:
+    var new_dict: Dictionary[String, Variant] = super.get_dict()
     new_dict["disable_active_abilities"] = self.disable_active_abilities
 
     return new_dict
 
-func restore_from_state(state):
+func restore_from_state(state: Dictionary) -> void:
     super.restore_from_state(state)
     self.disable_active_abilities = state["disable_active_abilities"]
 
-func is_hero():
+func is_hero() -> bool:
     return true
 
-func _apply_experience_modifiers(stats):
+func _apply_experience_modifiers(stats: Dictionary) -> Dictionary:
     if self.level > 0:
         stats["armor"] += 1
     if self.level > 1:

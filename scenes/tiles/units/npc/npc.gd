0e3e03ce945c78034b8ad286e152d39f80015abe
Yurii Mazurevich <JMazurevich@gmail.com>
2026-05-17 20:48:25 +0300

    Static typing: scenes/tiles/units/npc

--- scenes/tiles/units/npc/npc.gd
+++ scenes/tiles/units/npc/npc.gd
@@ -1,21 +1,20 @@
-extends "res://scenes/tiles/units/unit.gd"
+extends BaseUnit
 
-func can_attack(_unit):
+func can_attack(_unit: BaseUnit) -> bool:
 	return false
 
-func set_side_material(material):
+func set_side_material(material: Resource) -> void:
 	if material == null:
 		return
 
 	super.set_side_material(material)
 	$"mesh_anchor/standard".set_surface_override_material(0, material)
 
-func disable_shadow():
+func disable_shadow() -> void:
 	super.disable_shadow()
 
 	$"mesh_anchor/standard".cast_shadow = 0
 
-func reset_position_for_tile_view():
+func reset_position_for_tile_view() -> void:
 	super.reset_position_for_tile_view()
 	$"mesh_anchor/standard".hide()
-

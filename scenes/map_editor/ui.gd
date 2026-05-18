00d3a127e5e976beebefb5113aafb49c28eac04d
Yurii Mazurevich <JMazurevich@gmail.com>
2026-05-18 13:30:39 +0300

    Static typing: map editor ui

--- scenes/map_editor/ui.gd
+++ scenes/map_editor/ui.gd
@@ -1,57 +1,58 @@
 extends Control
+class_name MapEditorUi
 
 @onready var settings := Settings
 
 @onready var radial: Radial = $"radial/radial"
-@onready var picker = $"picker/picker"
-@onready var controls = $"controls/editor"
-@onready var story = $"story/StoryEditor"
-@onready var minimap = $"minimap"
-@onready var minimap_animations = $"minimap/animations"
+@onready var picker: MapPickerPanel = $"picker/picker"
+@onready var controls: Control = $"controls/editor"
+@onready var story: MapEditorStoryEditor = $"story/StoryEditor"
+@onready var minimap: MinimapView = $"minimap"
+@onready var minimap_animations: AnimationPlayer = $"minimap/animations"
 
-@onready var position_label = $"position/label"
-@onready var map_name_wrapper = $"map_name"
-@onready var map_name_label = $"map_name/inner/label"
+@onready var position_label: Label = $"position/label"
+@onready var map_name_wrapper: MarginContainer = $"map_name"
+@onready var map_name_label: Label = $"map_name/inner/label"
 
-@onready var tile_animations = $"tile/animations"
-@onready var tile_prev = $"tile/wrapper/tile_view_prev"
-@onready var tile_current = $"tile/wrapper/tile_view_current"
-@onready var tile_next = $"tile/wrapper/tile_view_next"
-@onready var type_prev = $"tile/wrapper/tile_type_prev"
-@onready var type_next = $"tile/wrapper/tile_type_next"
+@onready var tile_animations: AnimationPlayer = $"tile/animations"
+@onready var tile_prev: TileView = $"tile/wrapper/tile_view_prev"
+@onready var tile_current: TileView = $"tile/wrapper/tile_view_current"
+@onready var tile_next: TileView = $"tile/wrapper/tile_view_next"
+@onready var type_prev: TileView = $"tile/wrapper/tile_type_prev"
+@onready var type_next: TileView = $"tile/wrapper/tile_type_next"
 
 # Edge pan
-@onready var edge_pan_left = $"edge_pan/left"
-@onready var edge_pan_right = $"edge_pan/right"
-@onready var edge_pan_top = $"edge_pan/top"
-@onready var edge_pan_bottom = $"edge_pan/bottom"
+@onready var edge_pan_left: Control = $"edge_pan/left"
+@onready var edge_pan_right: Control = $"edge_pan/right"
+@onready var edge_pan_top: Control = $"edge_pan/top"
+@onready var edge_pan_bottom: Control = $"edge_pan/bottom"
 
-var icons = preload("res://scenes/ui/icons/icons.gd").new()
+var icons: IconsFactory = IconsFactory.new()
 
-func _ready():
+func _ready() -> void:
 	self.map_name_label.set_message_translation(false)
 	self.map_name_label.notification(NOTIFICATION_TRANSLATION_CHANGED)
 	self.show_controls()
 
-func update_position(x, y):
+func update_position(x: int, y: int) -> void:
 	self.position_label.set_text("[" + str(x) + ", " + str(y) + "]")
 
-func set_tile_prev(tile, t_rotation):
+func set_tile_prev(tile: MapObject, t_rotation: int) -> void:
 	self.tile_prev.set_tile(tile, t_rotation)
 
-func set_tile_current(tile, t_rotation):
+func set_tile_current(tile: MapObject, t_rotation: int) -> void:
 	self.tile_current.set_tile(tile, t_rotation)
 
-func set_tile_next(tile, t_rotation):
+func set_tile_next(tile: MapObject, t_rotation: int) -> void:
 	self.tile_next.set_tile(tile, t_rotation)
 
-func set_type_prev(tile, t_rotation):
+func set_type_prev(tile: MapObject, t_rotation: int) -> void:
 	self.type_prev.set_tile(tile, t_rotation)
 
-func set_type_next(tile, t_rotation):
+func set_type_next(tile: MapObject, t_rotation: int) -> void:
 	self.type_next.set_tile(tile, t_rotation)
 
-func toggle_radial():
+func toggle_radial() -> void:
 	if self.radial.is_visible():
 		self.hide_radial()
 		self.show_tiles()
@@ -62,53 +63,53 @@ func toggle_radial():
 		self.hide_position()
 
 
-func show_tiles():
+func show_tiles() -> void:
 	self.tile_animations.play("show")
 	self.show_minimap()
 	self.show_controls()
 
-func hide_tiles():
+func hide_tiles() -> void:
 	self.tile_animations.play("hide")
 	self.hide_minimap()
 	self.hide_controls()
 
-func show_minimap():
+func show_minimap() -> void:
 	self.minimap_animations.play("show")
 	if self.map_name_label.get_text() != "":
 		self.map_name_wrapper.show()
 
-func hide_minimap():
+func hide_minimap() -> void:
 	self.minimap_animations.play("hide")
 	self.map_name_wrapper.hide()
 
-func show_radial():
+func show_radial() -> void:
 	self.radial.show_menu()
 
-func hide_radial():
+func hide_radial() -> void:
 	self.radial.hide_menu()
 
-func show_picker():
+func show_picker() -> void:
 	self.picker.show_picker()
 
-func hide_picker():
+func hide_picker() -> void:
 	self.picker.hide_picker()
 
-func show_position():
+func show_position() -> void:
 	self.position_label.show()
 
-func hide_position():
+func hide_position() -> void:
 	self.position_label.hide()
 
-func close_all_popups():
+func close_all_popups() -> void:
 	if self.picker.is_visible():
 		self.hide_picker()
 	if self.story.is_visible():
 		self.hide_story()
 
-func is_radial_open():
+func is_radial_open() -> bool:
 	return self.radial.is_visible()
 
-func is_popup_open():
+func is_popup_open() -> bool:
 	if self.picker.is_visible():
 		return true
 	if self.story.is_visible():
@@ -116,7 +117,7 @@ func is_popup_open():
 
 	return false
 
-func is_panel_open():
+func is_panel_open() -> bool:
 	if self.radial.is_visible():
 		return true
 	if self.is_popup_open():
@@ -124,7 +125,7 @@ func is_panel_open():
 
 	return false
 
-func set_map_name(map_name, skip_show=false):
+func set_map_name(map_name: String, skip_show: bool = false) -> void:
 	self.map_name_label.set_text(map_name)
 	self.picker.set_map_name(map_name)
 
@@ -133,22 +134,22 @@ func set_map_name(map_name, skip_show=false):
 	else:
 		self.map_name_wrapper.hide()
 
-func load_minimap(map_name):
+func load_minimap(map_name: String) -> void:
 	self.minimap.remove_from_cache(map_name)
 	self.minimap.fill_minimap(map_name)
 
-func wipe_minimap():
+func wipe_minimap() -> void:
 	self.minimap.wipe()
 
-func show_controls():
+func show_controls() -> void:
 	if self.settings.get_option("show_controls"):
 		self.controls.show()
 
-func hide_controls():
+func hide_controls() -> void:
 	self.controls.hide()
 
-func show_story():
+func show_story() -> void:
 	self.story.show_panel()
 
-func hide_story():
+func hide_story() -> void:
 	self.story.hide()
--- scenes/ui/minimap/minimap.gd
+++ scenes/ui/minimap/minimap.gd
@@ -1,4 +1,5 @@
 extends Control
+class_name MinimapView
 
 @onready var map_list_service := MapManager
 @onready var minimap := $"minimap"

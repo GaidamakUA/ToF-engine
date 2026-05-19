b6bd5ae81e5eee08213a93c50bf1fccf5ca8afc3
Yurii Mazurevich <JMazurevich@gmail.com>
2026-05-19 11:21:33 +0300

    Fixed online warnings

--- scenes/ui/online/online.gd
+++ scenes/ui/online/online.gd
@@ -5,49 +5,49 @@ class_name OnlinePanel
 @onready var settings: SettingsService = Settings as SettingsService
 @onready var relay: RelayService = Relay as RelayService
 
-@onready var back_button = $"widgets/back_button"
+@onready var back_button: TextureButton = $"widgets/back_button"
 
-@onready var register_panel = $"widgets/register"
-@onready var register_button = $"widgets/register/register_button"
-@onready var register_description = $"widgets/register/description"
+@onready var register_panel: Control = $"widgets/register"
+@onready var register_button: TextureButton = $"widgets/register/register_button"
+@onready var register_description: Label = $"widgets/register/description"
 
-@onready var online_panel = $"widgets/online"
-@onready var maps_panel = $"widgets/online/maps"
-@onready var maps_main = $"widgets/online/maps/main"
-@onready var upload_button = $"widgets/online/maps/main/upload_button"
-@onready var download_button = $"widgets/online/maps/main/download_button"
+@onready var online_panel: Control = $"widgets/online"
+@onready var maps_panel: Control = $"widgets/online/maps"
+@onready var maps_main: Control = $"widgets/online/maps/main"
+@onready var upload_button: TextureButton = $"widgets/online/maps/main/upload_button"
+@onready var download_button: TextureButton = $"widgets/online/maps/main/download_button"
 
-@onready var maps_upload = $"widgets/online/maps/upload"
-@onready var confirm_upload_button = $"widgets/online/maps/upload/confirm_upload_button"
-@onready var confirm_upload_button_label = $"widgets/online/maps/upload/confirm_upload_button/label"
-@onready var change_button = $"widgets/online/maps/upload/change_button"
-@onready var upload_desc = $"widgets/online/maps/upload/description"
-@onready var upload_name = $"widgets/online/maps/upload/name"
+@onready var maps_upload: Control = $"widgets/online/maps/upload"
+@onready var confirm_upload_button: TextureButton = $"widgets/online/maps/upload/confirm_upload_button"
+@onready var confirm_upload_button_label: Label = $"widgets/online/maps/upload/confirm_upload_button/label"
+@onready var change_button: TextureButton = $"widgets/online/maps/upload/change_button"
+@onready var upload_desc: Label = $"widgets/online/maps/upload/description"
+@onready var upload_name: Label = $"widgets/online/maps/upload/name"
 
-@onready var maps_download = $"widgets/online/maps/download"
-@onready var retry_download_button = $"widgets/online/maps/download/retry_button"
-@onready var download_desc = $"widgets/online/maps/download/description"
-@onready var download_name = $"widgets/online/maps/download/name"
-
-
-
-
-@onready var online_match_nickname_input = $"widgets/online/maps/main/nickname"
-@onready var online_match_create_button = $"widgets/online/maps/main/create_button"
-@onready var online_match_join_code_input = $"widgets/online/maps/main/join_code"
-@onready var online_match_join_button = $"widgets/online/maps/main/join_button"
-
-@onready var online_match_connecting_panel = $"widgets/online/maps/busy"
+@onready var maps_download: Control = $"widgets/online/maps/download"
+@onready var retry_download_button: TextureButton = $"widgets/online/maps/download/retry_button"
+@onready var download_desc: Label = $"widgets/online/maps/download/description"
+@onready var download_name: Label = $"widgets/online/maps/download/name"
 
 
 
 
-var working = false
+@onready var online_match_nickname_input: LineEdit = $"widgets/online/maps/main/nickname"
+@onready var online_match_create_button: TextureButton = $"widgets/online/maps/main/create_button"
+@onready var online_match_join_code_input: LineEdit = $"widgets/online/maps/main/join_code"
+@onready var online_match_join_button: TextureButton = $"widgets/online/maps/main/join_button"
 
-var selected_upload_map = null
-var selected_download_map = null
+@onready var online_match_connecting_panel: Control = $"widgets/online/maps/busy"
 
-func _ready():
+
+
+
+var working: bool = false
+
+var selected_upload_map: Variant = null
+var selected_download_map: Variant = null
+
+func _ready() -> void:
     super._ready()
     self.upload_name.set_message_translation(false)
     self.upload_name.notification(NOTIFICATION_TRANSLATION_CHANGED)
@@ -74,7 +74,7 @@ func _on_back_button_pressed():
     else:
         self.main_menu.close_online()
 
-func _on_register_button_pressed():
+func _on_register_button_pressed() -> void:
     if self.working:
         return
 
@@ -84,7 +84,7 @@ func _on_register_button_pressed():
     self.register_button.hide()
     self.back_button.hide()
 
-    var result = await self.online.request_player_id()
+    var result: String = await self.online.request_player_id()
 
     self.working = false
     if result == "ok":
@@ -101,29 +101,29 @@ func _on_register_button_pressed():
 
 
 
-func show_panel():
+func show_panel() -> void:
     super.show_panel()
     self._select_panel()
 
-func _select_panel():
+func _select_panel() -> void:
     self.back_button.show()
     if self.online.is_integrated():
         self._configure_online_panel()
     else:
         self._configure_registration_panel()
 
-func _configure_online_panel():
+func _configure_online_panel() -> void:
     self.online_panel.show()
     self.register_panel.hide()
     self._configure_maps_panel()
 
-func _hide_all_subpanels():
+func _hide_all_subpanels() -> void:
     self.maps_main.hide()
     self.maps_upload.hide()
     self.maps_download.hide()
     self.online_match_connecting_panel.hide()
 
-func _configure_maps_panel():
+func _configure_maps_panel() -> void:
     self._hide_all_subpanels()
     if self.selected_upload_map != null:
         self._configure_maps_upload_confirm_panel()
@@ -133,13 +133,13 @@ func _configure_maps_panel():
         self._configure_maps_upload_panel()
 
 
-func _configure_maps_upload_panel():
+func _configure_maps_upload_panel() -> void:
     self.maps_main.show()
     await self.get_tree().create_timer(0.1).timeout
     self.upload_button.grab_focus()
 
 
-func _configure_maps_upload_confirm_panel():
+func _configure_maps_upload_confirm_panel() -> void:
     self.maps_upload.show()
     self.change_button.show()
     self.confirm_upload_button.show()
@@ -150,14 +150,14 @@ func _configure_maps_upload_confirm_panel():
     self.confirm_upload_button.grab_focus()
 
 
-func _configure_maps_download_panel():
+func _configure_maps_download_panel() -> void:
     self.maps_download.show()
     self.download_desc.set_text(tr("TR_DOWNLOADING_MAP"))
     self.download_name.set_text(self.selected_download_map)
     self._on_retry_button_pressed()
 
 
-func _configure_registration_panel():
+func _configure_registration_panel() -> void:
     self.online_panel.hide()
     self.register_panel.show()
     self.register_button.show()
@@ -166,24 +166,24 @@ func _configure_registration_panel():
     await self.get_tree().create_timer(0.1).timeout
     self.register_button.grab_focus()
 
-func _show_online_connecting_panel():
+func _show_online_connecting_panel() -> void:
     self._hide_all_subpanels()
     self.online_match_connecting_panel.show()
     self.back_button.hide()
 
 
-func _on_upload_button_pressed():
+func _on_upload_button_pressed() -> void:
     self.audio.play("menu_click")
     self.main_menu.open_upload_picker()
 
 
-func _on_download_button_pressed():
+func _on_download_button_pressed() -> void:
     self.audio.play("menu_click")
     self.main_menu.open_download_picker()
     self.back_button.hide()
 
 
-func _on_confirm_upload_button_pressed():
+func _on_confirm_upload_button_pressed() -> void:
     if self.working:
         return
 
@@ -194,7 +194,7 @@ func _on_confirm_upload_button_pressed():
     self.change_button.hide()
     self.back_button.hide()
 
-    var result = await self.online.upload_map(self.selected_upload_map)
+    var result: String = await self.online.upload_map(self.selected_upload_map)
 
     self.working = false
     self.back_button.show()
@@ -212,7 +212,7 @@ func _on_confirm_upload_button_pressed():
         self.confirm_upload_button.grab_focus()
 
 
-func _on_retry_button_pressed():
+func _on_retry_button_pressed() -> void:
     if self.working:
         return
 
@@ -222,7 +222,7 @@ func _on_retry_button_pressed():
     self.retry_download_button.hide()
     self.back_button.hide()
 
-    var result = await self.online.download_map(self.selected_download_map)
+    var result: bool = await self.online.download_map(String(self.selected_download_map))
 
     self.working = false
     self.back_button.show()
@@ -238,16 +238,16 @@ func _on_retry_button_pressed():
         self.retry_download_button.grab_focus()
 
 
-func _on_nickname_focus_exited():
+func _on_nickname_focus_exited() -> void:
     self.settings.set_option("nickname", self.online_match_nickname_input.get_text())
 
 
-func _on_create_button_pressed():
+func _on_create_button_pressed() -> void:
     self.audio.play("menu_click")
     self.main_menu.open_online_match_map_picker()
 
 
-func _on_join_button_pressed():
+func _on_join_button_pressed() -> void:
     self.audio.play("menu_click")
 
     if self.online_match_join_code_input.get_text() == "":
@@ -257,16 +257,16 @@ func _on_join_button_pressed():
     self._show_online_connecting_panel()
     self.relay.connect_game(self.online_match_join_code_input.get_text())
 
-func init_match(map_name):
+func init_match(map_name: String) -> void:
     self._show_online_connecting_panel()
     self.working = true
     self.relay.create_game(map_name)
 
-func _on_session_success():
+func _on_session_success() -> void:
     self.working = false
     self.main_menu.open_online_lobby()
 
-func _on_connection_failed():
+func _on_connection_failed() -> void:
     self.working = false
     self.back_button.show()
     self._select_panel()
